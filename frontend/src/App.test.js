a9f5ab8a534d4ebbe057c590ffbebe9a83add68c
Marcelo Inocente <menyeath@hotmail.com>
2023-03-14 01:41:27 -0500

    Deploy to EBS

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page title', () => {
   render(<App />);
   const linkElement = screen.getByText(/This is a test app/i);
   expect(linkElement).toBeInTheDocument();
